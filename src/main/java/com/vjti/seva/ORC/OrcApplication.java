64e30df31aa8028b4b96ca4dcf0d8285769ddf93
Shivanee Shinde <shivanee.shinde@oracle.com>
2020-05-20 12:57:59 +0530

    first commit

---
+++ src/main/java/com/vjti/seva/ORC/OrcApplication.java
@@ -0,0 +1,29 @@
+package com.vjti.seva.ORC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class OrcApplication {
+
+	@RequestMapping("/")
+    String hello() {
+        return "Hello World! JavaInterviewPoint";
+    }
+	@RequestMapping(method=RequestMethod.GET, path="/hello") 
+    public String helloworld() {
+        return "Hello World!";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vjti/seva/ORC/OrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjti.seva.ORC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
