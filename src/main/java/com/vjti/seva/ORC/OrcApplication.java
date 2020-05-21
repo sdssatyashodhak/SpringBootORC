829036edcc59c90ced06966645a30cc25e046156
Shivanee Shinde <shivanee.shinde@oracle.com>
2020-05-21 21:39:21 +0530

    third commit

--- src/main/java/com/vjti/seva/ORC/OrcApplication.java
+++ src/main/java/com/vjti/seva/ORC/OrcApplication.java
@@ -3,12 +3,13 @@ package com.vjti.seva.ORC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 @SpringBootApplication
 public class OrcApplication {
